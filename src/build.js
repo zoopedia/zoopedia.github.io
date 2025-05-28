44352383584bfe36e20f97874066bfe6d292a6fa
Jamie Humphries <jamie.humphries@outlook.com>
2025-05-28 16:46:27 +0100

    Table styling and images

--- src/build.js
+++ src/build.js
@@ -1,24 +1,36 @@
-import { writeFile } from "fs/promises";
+import { readFile, writeFile } from "fs/promises";
 import nunjucks from "nunjucks";
 import { join } from "path";
 import * as prettier from "prettier";
+import { compileString } from "sass";
 
 import { animals } from "../data/animals.js";
 
 const src = import.meta.dirname;
 const root = "docs";
 const views = ["templates", "views"].map((dir) => join(src, dir));
-nunjucks.configure(views);
+const env = nunjucks.configure(views);
 
 async function build() {
+  const css = await buildCss();
+  await writeFile(join(root, "css/main.css"), css);
+
   const html = await buildHtml();
-  const path = join(root, "index.html");
-  await writeFile(path, html);
+  await writeFile(join(root, "index.html"), html);
+}
+
+async function buildCss() {
+  const sass = await readFile(join(src, "styles/main.scss"));
+  return compileString(sass.toString()).css;
 }
 
 async function buildHtml() {
   const html = nunjucks.render("index.njk", { animals });
   return await prettier.format(html, { parser: "html" });
 }
+
+env.addFilter("localeString", function (number) {
+  return number.toLocaleString();
+});
 
 await build();
